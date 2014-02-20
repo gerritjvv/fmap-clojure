2da82d48611f43a44dc84557ed5d3cf76dfc6e45
gerritjvv <gerritjvv@gmail.com>
2014-02-20 17:14:32 +0100

    add javac 1.6

--- fmap-clojure/project.clj
+++ fmap-clojure/project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/gerritjvv/fmap-clojure/tree/master/fmap-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
- 
+
+  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
+
   :plugins [
          [lein-midje "3.0.1"] [lein-marginalia "0.7.1"]
          [lein-kibit "0.0.8"] [no-man-is-an-island/lein-eclipse "2.0.0"]
